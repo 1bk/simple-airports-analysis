a5e5eea3dc31f8793a13824f2f66004c72615afa
Boon Keong Lee <blee@moneylion.com>
2020-05-01 19:34:24 +0800

    Reformat and add date safe guard in `fct_arrivals__malaysia_summary`

--- dbt/models/core/fct_arrivals__malaysia_summary.sql
+++ dbt/models/core/fct_arrivals__malaysia_summary.sql
@@ -19,6 +19,12 @@ stg_arrivals__malaysia as (
 
 ),
 
+latest_arrival_date as (
+
+    select max(arrival_date) as latest from stg_arrivals__malaysia
+
+),
+
 aggregate as (
 
     select
@@ -27,11 +33,15 @@ aggregate as (
         arrival_icao,
         arrival_airport_name,
         count(*) as flight_count
+
     from
         stg_arrivals__malaysia
+
     where
         is_code_share is null
+
     {{ group_by(4) }}
+
     order by 5 desc
 
 ),
@@ -39,18 +49,26 @@ aggregate as (
 final as (
 
     select
-        a.airport_id,
-        a."name",
-        a.latitude,
-        a.longitude,
+        a.airport_id                as airport_id,
+        a."name"                    as "name",
+        a.latitude                  as latitude,
+        a.longitude                 as longitude,
         coalesce(b.flight_count, 0) as "flight_count"
+
     from
         base_airports a
-    left join
-        aggregate b
-    on
-       a.iata = b.arrival_iata
-    where country = 'Malaysia'
+
+        left join
+            aggregate b
+        on
+           a.iata = b.arrival_iata
+
+    where
+        a.country = 'Malaysia'
+    and
+        b.arrival_date in (
+            select latest from latest_arrival_date
+        )
 
 )
 
